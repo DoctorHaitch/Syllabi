f70d8efa1609ed395e5b7c831d3a6c2042ca2acd
KrisTheNerd <87826594+KrisTheNerd@users.noreply.github.com>
2025-05-08 23:19:49 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,43 +7,52 @@ var ptx_lunr_docs = [
   "type": "Front Matter",
   "number": "",
   "title": "Front Matter",
-  "body": "  Author X. Lastname    This is the abstract.   "
+  "body": "  Kris Hollingsworth, Ph.D (Dr. )    This is a living syllabus for the Fall 2025 semester at Minnesota State University, Mankato. Last updated, May 8, 2025. The latest policies for every course may be found here. Please contact me at kristopher.hollingsworth at mnsu.edu with any errors so that it may be appropriately updated.   As a living syllabus, this document is subject to change.  "
+},
+{
+  "id": "section-0",
+  "level": "1",
+  "url": "section-0.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor:  Kris Hollingsworth, Ph.D (Dr. )    Preferred Pronouns:  They\/Them    Office:  Wissink Hall (WH) 253    Email:  kristopher.hollingsworth at mnsu.edu    Phone:  507.389.6505        Office Hours    Mondays:  12:45-1:45pm    Tuesdays:  10:15am-1:45pm    Thursdays:  10:15am-1:45pm    Fridays:  12:45-1:45pm    Good ways to use office hours:  Get help on assigned homework problems.  Seek clarification on material presented during class.  Get academic\/career advice.  Discuss connections between current material and material from other courses.  Get advice on time management or effective study methods for the course.    "
+},
+{
+  "id": "section-0-2-1",
+  "level": "2",
+  "url": "section-0.html#section-0-2-1",
+  "type": "Table",
+  "number": "1.1",
+  "title": "",
+  "body": "   Professor:  Kris Hollingsworth, Ph.D (Dr. )    Preferred Pronouns:  They\/Them    Office:  Wissink Hall (WH) 253    Email:  kristopher.hollingsworth at mnsu.edu    Phone:  507.389.6505    "
+},
+{
+  "id": "section-0-3-1",
+  "level": "2",
+  "url": "section-0.html#section-0-3-1",
+  "type": "Table",
+  "number": "1.2",
+  "title": "Office Hours",
+  "body": " Office Hours    Mondays:  12:45-1:45pm    Tuesdays:  10:15am-1:45pm    Thursdays:  10:15am-1:45pm    Fridays:  12:45-1:45pm    "
 },
 {
   "id": "section-1",
   "level": "1",
   "url": "section-1.html",
   "type": "Section",
-  "number": "1",
-  "title": "My First Section",
-  "body": " My First Section   Some words    My First Subsection  Some more words  foo   A frog   A nice-looking frog with a longish description.    Bar:   "
-},
-{
-  "id": "subsection-1-4",
-  "level": "2",
-  "url": "section-1.html#subsection-1-4",
-  "type": "Figure",
-  "number": "1.1",
-  "title": "",
-  "body": " A frog   A nice-looking frog with a longish description.   "
-},
-{
-  "id": "section-2",
-  "level": "1",
-  "url": "section-2.html",
-  "type": "Section",
   "number": "2",
-  "title": "My Additional Section",
-  "body": " My Additional Section  foo   The graph made by TikZ   A 5-cycle     bar  "
+  "title": "Welcome to the Course",
+  "body": " Welcome to the Course   This section introduces policies which apply to every course I teach.    Equity and Inclusion   I want you to succeed in this class! As your professor I pledge to work to create an equitable learning environment, where all students belong. Mathematics can seem like the most objective of school subjects, but like all education, mathematics education is a cultural activity.  How we experience the world outside of math classes affect how we experience math classes. There is considerable research on how students` \\emph{sense of belonging} in their math classes impacts their learning. Feeling like you don't belong in a class can impact your cognitive load and diminish your ability to focus on the mathematics; so for example, you might use your brain to worry whether the professor or your classmates will think you are stupid if you make a mistake; to deal with the impact of a racist, sexist, classist, xenophobic, homophobic, transphobic, or ableist microaggression; or to wonder whether the other students are better than you.  William Yslas Vélez said, ``If a student finds pleasure and joy in finding a solution, in understanding a new concept, then this emotion is a sign of belonging to the mathematical community\" ( Imposter Syndrome , AMS e-Mentoring Network, posted 1\/25\/2019). Although frustration and struggle are also part of the mathematical enterprise, ultimately I see my work as setting up situations where all students have the opportunity to experience the joy of mathematics, to experience a sense of belonging, and to use our brains for learning math.    Student Diversity  The diversity of the participants in this course is a valuable source of ideas, problem solving strategies, and computational creativity. I consider this classroom to be a place where you will all be treated with respect, and I welcome individuals of all ages, backgrounds, beliefs, ethnicities, genders, gender identities, gender expressions, national origins, religious affiliations, sexual orientations, ability – and other visible and nonvisible differences. All members of this class are expected to contribute to a respectful, welcoming, and inclusive environment for every other member of the class.    Commitment to Anti-Racist Pedagogy  (Adapted from White Supremacy Culture Still Here by Tema Okun)  White supremacy refers to the ways in which the ruling class elite or the power elite in the colonies of what was to become the United States used the pseudo-scientific concept of race to create whiteness and a hierarchy of racialized value in order to:  disconnect and divide white people from Black, Indigenous, and People of Color (BIPOC);  disconnect and divide Black, Indigenous, and People of Color from each other;  disconnect and divide white people from other white people;  disconnect and divide each and all of us from the earth, the sun, the wind, the water, the stars, the animals that roam(ed) the earth;  disconnect and divide each of us from ourselves and from Keanu Reeves .  With a commitment to dismantling white supremacy, in my courses we will :  learn to admit when we are wrong. Understand that vulnerability can be a strength, particularly if you are sitting in a position of power and privilege;  understand that discomfort is at the root of all growth and learning;  welcome discomfort and learn to sit with discomfort before responding or acting;  avoid taking everything personally; we are all just human, glory and warts and genius and faults and all;  create a culture where people feel they can bring problems to the group; use meetings as a place to solve problems, not just a place to report activities;  create a culture of inquiry about what constitutes the \"right way\" and what defines a \"mistake\";  develop your skills individually and collectively to gather information through different ways of knowing; appreciate when others learn from and expand on our knowledge and writing; see ourselves and position ourselves as one person in a stream of knowing and learning;  remember that feedback may be skillful or unskillful and either way, it will not kill you;  understand the link between defensiveness and fear (of losing power, losing face, losing comfort, losing privilege); work on our own defensiveness; ask ourselves what we are defending against and why;  learn to recognize those times when you need to go off the planned agenda in order to address people's underlying concerns with the knowledge that doing so will result in a more solid product in the long term; understand that everything takes longer than anyone expects.     "
 },
 {
-  "id": "figure-tikz-example-diagram",
-  "level": "2",
-  "url": "section-2.html#figure-tikz-example-diagram",
-  "type": "Figure",
-  "number": "2.1",
-  "title": "",
-  "body": " The graph made by TikZ   A 5-cycle    "
+  "id": "section-3a",
+  "level": "1",
+  "url": "section-3a.html",
+  "type": "Section",
+  "number": "3",
+  "title": "MATH122 - Calculus 2",
+  "body": " MATH122 - Calculus 2   This course will cover topics in (official course catalog description) techniques of integration, applications of integration, improper integrals, numerical integration, the calculus of parametric curves, and infinite series and sequences.    Why study calculus?  This rigorous mathematical subject consists of a basic mathematical tool kit for dealing with processes that change continuously. Part of the reason that calculus turns out to be so useful is that the details of the process turn out not to matter so much -- all that matters is that the process is continuous. In fact, if the increments are small enough, discrete processes can sometimes be approximated as continuous and treated using the techniques of calculus. More specifically, Calculus is the story of infinitely improving approximations concerned with two historical problems:  The slope of a \"curvy line\" at a point.  The area beneath a \"curvy line\".  Miraculously, these two problems, which may seem barely related except for the type of line they seek to study, turn out to be deeply intertwined. This semester, we will primarily focus on the second problem, but due to the dual nature of these problems the other is necessary and a skill which you hopefully developed during Calculus I.  In order to do well in MATH122 , you need to work on developing both conceptual understanding and algebraic fluency. Conceptual understanding is necessary as memorized facts are not much help if you do not know how to use them. Algebraic fluency is required as knowing the basic concepts is not very helpful if you are unable to easily use them because your knowledge is not built on a firm foundation of the governing principles.  By fluency, I mean that you can generally use them without thinking about them. Fluency is defined as the ability to speak or write a language easily and accurately . Similar to your fluency in your native language, where you likely are fluent in 10000-20000 words, you are expected to be fluent in a number of \"mathematical rules\" which you have learned over the last several years. Note, there is a full inventory of the algebraic, trigonometric, and Calculus I skills with which you need to be fluent in (TODO:INSERT REFERENCE HERE ONCE IT IS PORTED TO PRETEXT).    Learning Outcomes  Upon completion of this course students will have demonstrated  a working knowledge of Riemann integration theory and several specific applications. This will build off material from MATH121: Calculus I .  a working knowledge of sequences, series, and their relevance to calculus.  their ability to memorize, derive, and practice facts (the derivatives and antiderivatives of basic functions, for instance) and fundamental theorems.  understanding what is going on behind the symbols by developing the patterns of thought and the problem-solving skills needed when dealing with problems involving continuous change and difficult integrals. If you limit yourself to memorizing facts and formulas, you will miss out on this very important part of the course, which makes calculus so important for so many application areas.  team work skills by collaborating within a small group, towards completion of a common task or goal.     "
 }
 ]
 
